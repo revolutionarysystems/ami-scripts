8a5f45dfe4b3639dc70d855a3fb1b715a3e7291d
Adam Knight <adam@revolutionarysystems.co.uk>
2014-11-13 10:33:30 +0000

    fixed shutdown script

--- onshutdown.sh
+++ onshutdown.sh
@@ -7,13 +7,9 @@ else
     data=$(curl http://169.254.169.254/latest/user-data)
 fi
 echo data = $data
-alias=$(echo $data | cut -f1 -d\|)
-echo alias = $alias
-puppetmaster=$(echo $data | cut -f2 -d\|)
-echo puppetmaster = $puppetmaster
-zoneId=$(echo $data | cut -f3 -d\|)
+zoneId=$(echo $data | cut -f2 -d\|)
 echo zoneId = $zoneId
-domain=$(echo $data | cut -f4 -d\|)
+domain=$(echo $data | cut -f3 -d\|)
 echo domain = $domain
 if [ ! -z "$zoneId" ]
 then
